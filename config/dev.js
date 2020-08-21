ba5699deda84370a7a75b34561dbba75c65da319
Artur Straub <straubartur@gmail.com>
2020-08-21 12:14:38 -0300

    remove keys

--- config/dev.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  googleClientID:
-    '70265989829-0t7m7ce5crs6scqd3t0t6g7pv83ncaii.apps.googleusercontent.com',
-  googleClientSecret: '8mkniDQOqacXtlRD3gA4n2az',
-  mongoURI: 'mongodb://admin:D4t4b4s3@ds127958.mlab.com:27958/advancednode',
-  cookieKey: '123123123',
-};
